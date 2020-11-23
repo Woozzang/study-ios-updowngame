25eddfed51b8f17fb75f3c6918287c779f7891e4
Woozzang <woozzaang@gmail.com>
2020-11-24 02:29:23 +0900

    Added action in hitButton

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hitButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,6 +19,10 @@ class ViewController: UIViewController {
 
    @IBAction  func sliderValueChanged(_ sender: UISlider){
         print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton(_ sender: UIButton){
+        print(slider.value)
     }
 }
 
