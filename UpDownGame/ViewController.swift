ec22816d89fab8d389f60bc050028033585bd594
Woozzang <woozzaang@gmail.com>
2020-11-24 01:11:35 +0900

    receiving value changed event from slider

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+   @IBAction  func sliderValueChanged(_ sender: UISlider){
+        print(sender.value)
+    }
 }
 
